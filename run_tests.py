132672a93f8c7bf2f71de2f6d8e22574858dc1a7
Nickolay <lukaville@gmail.com>
2015-11-28 02:00:02 +0300

    check number of results

--- run_tests.py
+++ run_tests.py
@@ -3,18 +3,19 @@
 import sys
 import unittest
 
+from tests.buy_tests import CheckFilterTabs, CheckNumberOfResults
 from tests.review_test import CarSelectionTest, LoginTest, LogoutTest, AverageRatingTest, ReviewTextInputTest, \
     AddReviewTest, AddReviewErrorsTest
 
 if __name__ == '__main__':
     suite = unittest.TestSuite((
-        unittest.makeSuite(AddReviewErrorsTest),
+        # unittest.makeSuite(AddReviewErrorsTest),
         # unittest.makeSuite(LoginTest),
         # unittest.makeSuite(LogoutTest),
         # unittest.makeSuite(AverageRatingTest),
         # unittest.makeSuite(CarSelectionTest),
         # unittest.makeSuite(ReviewTextInputTest),
-        unittest.makeSuite(AddReviewTest)
+        unittest.makeSuite(CheckNumberOfResults)
     ))
     result = unittest.TextTestRunner().run(suite)
     sys.exit(not result.wasSuccessful())
